d4876ede2e2d7b785bf1df5cf53d0450d888ce80
c1foz <tomife2@gmail.com>
2024-02-24 18:20:39 +0100

    refactor gameobjects

--- Assets/Scripts/FrameRateManager.cs
+++ Assets/Scripts/FrameRateManager.cs
@@ -2,11 +2,11 @@ using UnityEngine;
 
 public class FramerateManager : MonoBehaviour
 {
-    [SerializeField] private int targetFPS = 60;
+    private readonly int targetFPS = 60;
 
     private void Awake()
     {
         Application.targetFrameRate = targetFPS;
-        QualitySettings.vSyncCount = 0; // Disable VSync for more precise control
+        QualitySettings.vSyncCount = 0;
     }
 }
