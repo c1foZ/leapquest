827ca66212f3e7e312459b863581e73cde907452
tomasferencik <tomife2@gmail.com>
2024-11-26 19:06:28 +0100

    fixing joystick

--- Assets/Scripts/MovementJoystick.cs
+++ Assets/Scripts/MovementJoystick.cs
@@ -5,53 +5,45 @@ public class MovementJoystick : MonoBehaviour
 {
     public GameObject joystick;
     public GameObject joystickArea;
-    public Vector2 joystickVec; 
-    private Vector2 joystickTouchPos; 
-    private float joystickRadius; 
-    private readonly Vector2 exactPosition = new Vector2(100, 130); 
+    public Vector2 joystickVec;
+    private Vector2 joystickTouchPos;
+    private Vector2 joystickOriginalPos;
+    private float joystickRadius;
 
     void Start()
     {
+        joystickOriginalPos = joystick.transform.position;  // Keep track of joystick's original position.
         joystickRadius = joystickArea.GetComponent<RectTransform>().sizeDelta.y / 2;
-
-        ResetToExactPosition();
     }
 
     public void PointerDown()
     {
-        Vector2 mousePos = Input.mousePosition;
-        joystick.GetComponent<RectTransform>().position = mousePos;
-        joystickArea.GetComponent<RectTransform>().position = mousePos;
-        joystickTouchPos = mousePos;
+        // Don't move the joystick when the area is touched. Keep its position fixed.
+        joystickTouchPos = Input.mousePosition;
     }
 
     public void Drag(BaseEventData baseEventData)
     {
         PointerEventData pointerEventData = baseEventData as PointerEventData;
         Vector2 dragPos = pointerEventData.position;
-
         joystickVec = (dragPos - joystickTouchPos).normalized;
+
         float joystickDist = Vector2.Distance(dragPos, joystickTouchPos);
 
+        // Update the vector without changing the joystick's position.
         if (joystickDist < joystickRadius)
         {
-            joystick.GetComponent<RectTransform>().position = joystickTouchPos + joystickVec * joystickDist;
+            joystickVec = (dragPos - joystickTouchPos).normalized;
         }
         else
         {
-            joystick.GetComponent<RectTransform>().position = joystickTouchPos + joystickVec * joystickRadius;
+            joystickVec = (dragPos - joystickTouchPos).normalized * joystickRadius;
         }
     }
 
     public void PointerUp()
     {
+        // Reset the joystick vector to zero when touch is released.
         joystickVec = Vector2.zero;
-        ResetToExactPosition();
-    }
-
-    private void ResetToExactPosition()
-    {
-        joystick.GetComponent<RectTransform>().anchoredPosition = exactPosition;
-        joystickArea.GetComponent<RectTransform>().anchoredPosition = exactPosition;
     }
 }
