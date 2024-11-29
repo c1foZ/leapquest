a1df3f97550f71e4f30aa4f757331e501c92bac5
tomasferencik <tomife2@gmail.com>
2024-11-29 10:24:57 +0100

    fixing 0 health

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -39,7 +39,7 @@ public class GameManager : MonoBehaviour
 
         if (health == 0)
         {
-            QuitGame();
+            RestartGameFromMenu();
         }
 
         UpdateScoreText();
